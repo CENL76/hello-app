66a85a9e6f4254c052107fa110568028298fa0a0
CENL76 <carlos.noblecilla@alumnos.unini.edu.mx>
2022-07-03 21:09:52 -0500

    first commit

---
+++ hello.java
@@ -0,0 +1,43 @@
+public class hello {
+
+    /**
+     * @param args
+     */
+    public static void main ( String [] args ) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+        String frase = "Este es mi primer ejercicio en java";
+        String fraseDos = "Estamos  aprendiendo a programar en java " ;
+        String fraseTres = frase.concat(fraseDos);
+
+        System.out.println("Hello World");
+
+        System.out.println("maravilloso dia");
+
+        System.out.println("hoy empezamos denuevo");
+
+        System.out.println("nunca es tarde para empezar a aprender nuevas cosas");
+
+        System.out.println("la frase es :" + frase);
+
+        System.out.println("la frase - " + frase + " tiene -" + frase.length() + "- letras");
+
+        System.out.println(frase.charAt(3));
+
+        System.out.println(frase.toCharArray());
+
+        System.out.println(frase.toLowerCase()); 
+
+        System.out.println(frase.toUpperCase());
+
+        System.out.println(frase.isEmpty());
+
+        System.out.println(fraseTres) ;
+
+        System.out.println(frase.concat(fraseDos));
+        
+
+
+
+    }
+
+}
